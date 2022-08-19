0e79574acf299893f9574ae79f1f4797cd226a7a
Rivan <parmarrivan@gmail.com>
2022-08-19 20:34:46 +0530

    Initial commit.

---
+++ app/src/main/java/com/rivan/androidplaygrounds/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.rivan.androidplaygrounds
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rivan.androidplaygrounds.ui.theme.AndroidPlaygroundsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidPlaygroundsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidPlaygroundsTheme {
+        Greeting("Android")
+    }
+}
