ab7ddb08c9b999bcafe585ce661961ff1b0f80f4
Rivan <parmarrivan@gmail.com>
2022-10-16 14:16:58 +0530

    Completed Module.kt

--- build-tools/project/src/main/java/com/rivan/builder/project/api/Module.kt
+++ build-tools/project/src/main/java/com/rivan/builder/project/api/Module.kt
@@ -1,5 +1,44 @@
 package com.rivan.builder.project.api
 
-interface Module {
+import org.jetbrains.kotlin.com.intellij.openapi.util.UserDataHolderEx
+import java.io.File
+import java.io.IOException
+import java.util.*
 
+interface Module : UserDataHolderEx {
+
+    fun getRootFile(): File
+
+    fun getName(): String {
+        return getRootFile().name
+    }
+
+    /**
+     * Start parsing the project contents such as manifest data, project settings, etc.
+     *
+     * Implementations may throw an IOException if something went wrong during parsing
+     */
+    @Throws(IOException::class)
+    fun open()
+
+    /**
+     * Remove all the indexed files
+     */
+    fun clear()
+
+    /**
+     * Index all the files
+     */
+    fun index()
+
+    fun addChildModule(module: Module) {
+
+    }
+
+    /**
+     * Get the dependencies of the module
+     */
+    fun getModuleDependencies(): Set<String> {
+        return Collections.emptySet()
+    }
 }
