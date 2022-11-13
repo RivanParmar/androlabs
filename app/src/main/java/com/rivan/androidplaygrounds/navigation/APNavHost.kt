313aceeddbc63e4e668b3d44c58e33a280bfc181
Rivan <parmarrivan@gmail.com>
2022-11-13 22:48:15 +0530

    Added code to APNavHost.kt

--- app/src/main/java/com/rivan/androidplaygrounds/navigation/APNavHost.kt
+++ app/src/main/java/com/rivan/androidplaygrounds/navigation/APNavHost.kt
@@ -1,2 +1,26 @@
 package com.rivan.androidplaygrounds.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.rivan.androidplaygrounds.feature.recent.navigation.recentNavigationRoute
+import com.rivan.androidplaygrounds.feature.recent.navigation.recentScreen
+import com.rivan.androidplaygrounds.feature.settings.navigation.settingScreen
+
+@Composable
+fun APNavHost(
+    navController: NavHostController,
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    startDestination: String = recentNavigationRoute
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        recentScreen()
+        settingScreen()
+    }
+}
