4f3a639182cb61d4b15ffc13b45058242648db64
RivanParmar <parmarrivan@gmail.com>
2023-03-30 01:22:04 +0530

    Minor change in UserProjectResourceData.kt

--- core/model/src/main/kotlin/com/rivan/androlabs/core/model/data/UserProjectResourceData.kt
+++ core/model/src/main/kotlin/com/rivan/androlabs/core/model/data/UserProjectResourceData.kt
@@ -4,6 +4,7 @@ package com.rivan.androlabs.core.model.data
  * Class summarizing user's project resource data.
  */
 data class UserProjectResourceData(
+    val recentProjectResources: Set<String>,
     val favouriteProjectResources: Set<String>,
     val completedProjectResources: Set<String>
 )
