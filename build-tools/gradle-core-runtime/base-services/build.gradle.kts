dc1540ec0e9a51dcf0a5bb75fbf25b430b5555b5
RivanParmar <parmarrivan@gmail.com>
2023-10-17 01:22:41 +0530

    Added more code to `messaging`

--- build-tools/gradle-core-runtime/base-services/build.gradle.kts
+++ build-tools/gradle-core-runtime/base-services/build.gradle.kts
@@ -8,8 +8,8 @@ dependencies {
     implementation("commons-io:commons-io:2.14.0")
     implementation("javax.inject:javax.inject:1")
     implementation("org.ow2.asm:asm:9.6")
-    implementation("org.slf4j:slf4j-api:2.0.9")
 
     implementation(libs.apache.commons.lang)
     implementation(libs.guava)
+    implementation(libs.slf4j)
 }
--- build-tools/gradle-core-runtime/messaging/build.gradle.kts
+++ build-tools/gradle-core-runtime/messaging/build.gradle.kts
@@ -6,4 +6,10 @@ dependencies {
     implementation(project(":build-tools:gradle-core-runtime:base-annotations"))
     implementation(project(":build-tools:gradle-core-runtime:base-services"))
     implementation(project(":build-tools:gradle-build-operations"))
+
+    implementation(libs.guava)
+    implementation(libs.slf4j)
+
+    implementation("com.esotericsoftware:kryo:5.5.0")
+    implementation("it.unimi.dsi:fastutil:8.5.12")
 }
