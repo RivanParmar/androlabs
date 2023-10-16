f1391de9b961eaafa8fa1cefe0dcba0ab1f5e7dd
RivanParmar <parmarrivan@gmail.com>
2023-10-16 23:14:15 +0530

    Added more code to `base-annotations`

--- build-tools/gradle-core-runtime/base-services/build.gradle.kts
+++ build-tools/gradle-core-runtime/base-services/build.gradle.kts
@@ -5,5 +5,9 @@ plugins {
 dependencies {
     implementation(project(":build-tools:gradle-core-runtime:base-annotations"))
 
+    implementation("org.ow2.asm:asm:9.6")
+    implementation("org.slf4j:slf4j-api:2.0.9")
+
+    implementation(libs.apache.commons.lang)
     implementation(libs.guava)
 }
