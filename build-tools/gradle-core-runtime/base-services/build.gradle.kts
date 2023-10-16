30676f198a6dbe0249fa3ccdbd3b6eb68f3b595c
RivanParmar <parmarrivan@gmail.com>
2023-10-17 00:47:42 +0530

    Added more code to `base-services`

--- build-tools/gradle-core-runtime/base-services/build.gradle.kts
+++ build-tools/gradle-core-runtime/base-services/build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 dependencies {
     implementation(project(":build-tools:gradle-core-runtime:base-annotations"))
 
+    implementation("commons-io:commons-io:2.14.0")
+    implementation("javax.inject:javax.inject:1")
     implementation("org.ow2.asm:asm:9.6")
     implementation("org.slf4j:slf4j-api:2.0.9")
 
