2725a5371e35a38efcbecd96a5bb5d1ce3979883
RivanParmar <parmarrivan@gmail.com>
2023-10-17 15:54:24 +0530

    Added more code to `gradle-build-operations`

--- build-tools/gradle-build-operations/build.gradle.kts
+++ build-tools/gradle-build-operations/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     id("androlabs.jvm.library")
 }
+
+dependencies {
+    implementation(project(":build-tools:gradle-core-runtime:base-annotations"))
+
+    implementation(libs.slf4j)
+}
