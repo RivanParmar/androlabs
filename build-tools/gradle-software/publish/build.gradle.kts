7e98df46221b231a673539734c76d821209d7d8a
RivanParmar <parmarrivan@gmail.com>
2023-10-22 02:51:35 +0530

    Created `publish` module

---
+++ build-tools/gradle-software/publish/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("androlabs.jvm.library")
+}
+
+description = "Base plugin for the maven and ivy publish plugins. Defines the publishing extension."
+
+dependencies {
+    implementation(project(":build-tools:base-services"))
+    implementation(project(":build-tools:functional"))
+    implementation(project(":build-tools:logging"))
+    implementation(project(":build-tools:file-collections"))
+    implementation(project(":build-tools:gradle-core-api"))
+    implementation(project(":build-tools:model-core"))
+    implementation(project(":build-tools:gradle-core"))
+    implementation(project(":build-tools:base-services-groovy")) // for 'Specs'
+    implementation(project(":build-tools:dependency-management"))
+
+    implementation(libs.groovy)
+    implementation(libs.guava)
+    implementation(libs.apache.commons.lang3)
+    implementation(libs.gson)
+    implementation(libs.javax.inject)
+}
