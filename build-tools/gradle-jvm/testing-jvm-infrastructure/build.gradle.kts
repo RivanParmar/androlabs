34400da6d1a413a321d3692a8a0340fbca8d24df
RivanParmar <parmarrivan@gmail.com>
2023-10-22 15:45:38 +0530

    Created `testing-jvm-infrastructure` module

---
+++ build-tools/gradle-jvm/testing-jvm-infrastructure/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("androlabs.jvm.library")
+}
+
+description = """JVM-specific test infrastructure, including support for bootstrapping and configuring test workers
+and executing tests.
+Few projects should need to depend on this module directly. Most external interactions with this module are through the
+various implementations of WorkerTestClassProcessorFactory.
+"""
+
+dependencies {
+    implementation(project(":build-tools:base-services"))
+    implementation(project(":build-tools:messaging"))
+    implementation(project(":build-tools:testing-base"))
+
+    implementation("org.testng:testng:7.8.0")
+    implementation("org.apache-extras.beanshell:bsh:2.0b6") {
+        because("Used by TestNG")
+    }
+
+    implementation(libs.slf4j)
+    implementation(libs.apache.commons.lang3)
+    implementation(libs.junit4)
+}
