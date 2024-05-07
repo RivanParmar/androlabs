98e8445253c63da35a15dd6a7ebbcddb1ae18e22
RivanParmar <parmarrivan@gmail.com>
2024-05-07 23:40:03 +0530

    Updated the app to use the new edge-to-edge API and minor changes

--- app/src/main/java/com/rivan/androlabs/MainActivity.kt
+++ app/src/main/java/com/rivan/androlabs/MainActivity.kt
@@ -2,7 +2,9 @@ package com.rivan.androlabs
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
@@ -13,17 +15,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.WindowCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.google.accompanist.adaptive.calculateDisplayFeatures
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.rivan.androlabs.MainActivityUiState.Loading
 import com.rivan.androlabs.MainActivityUiState.Success
 import com.rivan.androlabs.core.designsystem.theme.AndrolabsTheme
 import com.rivan.androlabs.core.model.data.DarkThemeConfig
 import com.rivan.androlabs.ui.AndrolabsApp
+import com.rivan.androlabs.ui.rememberAndrolabsAppState
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
@@ -57,30 +58,43 @@ class MainActivity : ComponentActivity() {
         // the UI.
         splashScreen.setKeepOnScreenCondition {
             when (uiState) {
-                is Loading -> true
+                Loading -> true
                 is Success -> false
             }
         }
 
         // Turn off the decor fitting system windows, which allows us to handle insets,
-        // including IME animations
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        // including IME animations, and go edge-to-edge
+        // This also sets up the initial system bar style based on the platform theme
+        enableEdgeToEdge()
 
         setContent {
-            val systemUiController = rememberSystemUiController()
             val darkTheme = shouldUseDarkTheme(uiState)
 
-            DisposableEffect(systemUiController, darkTheme) {
-                systemUiController.statusBarDarkContentEnabled = !darkTheme
+            DisposableEffect(darkTheme) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        android.graphics.Color.TRANSPARENT,
+                        android.graphics.Color.TRANSPARENT,
+                    ) { darkTheme },
+                    navigationBarStyle = SystemBarStyle.auto(
+                        lightScrim,
+                        darkScrim,
+                    ) { darkTheme }
+                )
                 onDispose {}
             }
+            
+            val appState = rememberAndrolabsAppState(
+                windowSizeClass = calculateWindowSizeClass(this),
+                displayFeatures = calculateDisplayFeatures(this),
+            )
 
             AndrolabsTheme(
                 darkTheme = darkTheme
             ) {
                 AndrolabsApp(
-                    windowSizeClass = calculateWindowSizeClass(activity = this),
-                    displayFeatures = calculateDisplayFeatures(activity = this),
+                    appState = appState,
                     askToSelectSavePath = askToSelectSavePath(uiState),
                     updateSavePath = viewModel::updateSavePath,
                 )
@@ -89,9 +103,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * Returns `true` if dark theme should be used, as a function of the [uiState] and the
+ * current system context.
+ */
 @Composable
 private fun shouldUseDarkTheme(
-    uiState: MainActivityUiState
+    uiState: MainActivityUiState,
 ): Boolean = when (uiState) {
     Loading -> isSystemInDarkTheme()
     is Success -> when (uiState.userSettings.darkThemeConfig) {
@@ -102,8 +120,20 @@ private fun shouldUseDarkTheme(
 }
 
 private fun askToSelectSavePath(
-    uiState: MainActivityUiState
+    uiState: MainActivityUiState,
 ): Boolean = when (uiState) {
     Loading -> false
     is Success -> uiState.userSettings.savePath.isEmpty()
 }
+
+/**
+ * The default light scrim, as defined by androidx and the platform:
+ * https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:activity/activity/src/main/java/androidx/activity/EdgeToEdge.kt;l=35-38;drc=27e7d52e8604a080133e8b842db10c89b4482598
+ */
+private val lightScrim = android.graphics.Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
+
+/**
+ * The default dark scrim, as defined by androidx and the platform:
+ * https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:activity/activity/src/main/java/androidx/activity/EdgeToEdge.kt;l=40-44;drc=27e7d52e8604a080133e8b842db10c89b4482598
+ */
+private val darkScrim = android.graphics.Color.argb(0x80, 0x1b, 0x1b, 0x1b)
--- app/src/main/java/com/rivan/androlabs/ui/AndrolabsApp.kt
+++ app/src/main/java/com/rivan/androlabs/ui/AndrolabsApp.kt
@@ -1,18 +1,11 @@
 package com.rivan.androlabs.ui
 
-import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.window.layout.DisplayFeature
 import com.rivan.androlabs.navigation.MainActivityNavHost
 
 @Composable
 fun AndrolabsApp(
-    windowSizeClass: WindowSizeClass,
-    displayFeatures: List<DisplayFeature>,
-    appState: AndrolabsAppState = rememberAndrolabsAppState(
-        windowSizeClass = windowSizeClass,
-        displayFeatures = displayFeatures
-    ),
+    appState: AndrolabsAppState,
     askToSelectSavePath: Boolean,
     updateSavePath: (String) -> Unit,
 ) {
